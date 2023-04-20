870ab50a60123d4815fa64a515409b09f1c7e801
nvyooo <131399444+nvyooo@users.noreply.github.com>
2023-04-20 21:33:10 +0200

    Add files via upload

---
+++ budzert/jsadhfkjsdafk/jsadhfkjsdafk/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BudgetManager;
+
+namespace BudgetManagerProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Budget budget = new Budget();
+
+            while (true)
+            {
+                Console.WriteLine("Wprowadź transakcje (wartość(zł), opis(za co?), Czy to wydatek?(true,false) ):");
+                string input = Console.ReadLine();
+
+                if (input == "exit")
+                {
+                    break;
+                }
+
+                string[] parts = input.Split(' ');
+                decimal amount = decimal.Parse(parts[0]);
+                string description = parts[1];
+                bool isExpense = bool.Parse(parts[2]);
+                Transaction t = new Transaction(amount, DateTime.Now, description, isExpense);
+                budget.AddTransaction(t);
+                Console.Clear();
+            }
+
+            budget.PrintSummary();
+        }
+    }
+}
---
+++ budzert/jsadhfkjsdafk/jsadhfkjsdafk/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BudgetManager
+{
+    public class Transaction
+    {
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public bool IsExpense { get; set; }
+
+        public Transaction(decimal amount, DateTime date, string description, bool isExpense)
+        {
+            Amount = amount;
+            Date = date;
+            Description = description;
+            IsExpense = isExpense;
+        }
+    }
+
+    public class Budget
+    {
+        private List<Transaction> Transactions { get; set; }
+
+        public Budget()
+        {
+            Transactions = new List<Transaction>();
+        }
+
+        public void AddTransaction(Transaction transaction)
+        {
+            Transactions.Add(transaction);
+        }
+
+        public void RemoveTransaction(Transaction transaction)
+        {
+            Transactions.Remove(transaction);
+        }
+
+        public decimal GetIncome()
+        {
+            decimal income = 0;
+            foreach (var transaction in Transactions)
+            {
+                if (!transaction.IsExpense)
+                {
+                    income += transaction.Amount;
+                }
+            }
+            return income;
+        }
+
+        public decimal GetExpenses()
+        {
+            decimal expenses = 0;
+            foreach (var transaction in Transactions)
+            {
+                if (transaction.IsExpense)
+                {
+                    expenses += transaction.Amount;
+                }
+            }
+            return expenses;
+        }
+
+        public decimal GetBalance()
+        {
+            return GetIncome() - GetExpenses();
+        }
+
+        public void PrintSummary()
+        {
+            Console.WriteLine("Przychody: zł" + GetIncome());
+            Console.WriteLine("Wydatki: zł" + GetExpenses());
+            Console.WriteLine("Balans: zł" + GetBalance());
+            Console.WriteLine();
+            Console.WriteLine("Podsumowanie transakcji:");
+            Console.WriteLine("-------------------");
+            foreach (var transaction in Transactions)
+            {
+                Console.WriteLine(transaction.Date.ToShortDateString() + ": zł" + transaction.Amount + " - " + transaction.Description + " (" + (transaction.IsExpense ? "Wydatki" : "Przychody") + ")");
+            }
+        }
+
+    }
+}
